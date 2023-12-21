ed4fceaea1804418ff7ab7a8ca8272b8e0d8bd42
NatureUniverse <86906483+NatureUniverse@users.noreply.github.com>
2023-12-21 23:03:22 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='NaTu',
+  version='0.0.1',
+  description='Make python more eazy',
+  url='https://github.com/NatureUniverse/Python_Improvement_Library',
+  author='NatureUniverse',
+  author_email='pinokio082722@gmail.com',
+  license='MIT',
+  packages=find_packages(),
+  install_requires=['re'],
+)
